d08a0bc68e3548323ac899794b735c80c6372c0a
Jacopo <jacopo.degattis@gmail.com>
2025-07-27 22:55:35 +0200

    Read dab.yeet.su endpoint and download path from ENV variables

--- main.go
+++ main.go
@@ -4,11 +4,23 @@ import (
 	"flag"
 	"godab/api"
 	"log"
+	"os"
 )
 
 func main() {
+	serverEndpoint := os.Getenv("DAB_ENDPOINT")
+	downloadLocation := os.Getenv("DOWNLOAD_LOCATION")
+
+	if serverEndpoint == "" {
+		panic("You must provide a valid `DAB_ENDPOINT` env variable")
+	}
+
+	if downloadLocation == "" {
+		panic("You must provide a valid `DOWNLOAD_LOCATION` env variable")
+	}
+
 	// Replace URL and DOWNLOAD_PATH with a ENV variable
-	dapi := api.New("https://dab.yeet.su", "<DOWNLOAD_PATH>")
+	dapi := api.New(serverEndpoint, downloadLocation)
 
 	var (
 		album string
