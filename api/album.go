f3388f7f0a6526e30ab674b6aa3f95459dca1a09
csoftware-arigpt <ksoarigpt@gmail.com>
2026-01-20 09:18:22 +0300

    Fix code indentation

--- api/album.go
+++ api/album.go
@@ -1,203 +1,202 @@
 package api
 
 import (
-        "encoding/json"
-        "fmt"
-        "godab/config"
-        "math/rand"
-        "os"
-        "sync"
-        "time"
+	"encoding/json"
+	"fmt"
+	"godab/config"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
 
-        "github.com/jedib0t/go-pretty/v6/progress"
+	"github.com/jedib0t/go-pretty/v6/progress"
 )
 
 type Album struct {
-        Id          string  `json:"id"`
-        Title       string  `json:"title"`
-        Artist      string  `json:"artist"`
-        Cover       string  `json:"cover"`
-        ReleaseDate string  `json:"releaseDate"`
-        TrackCount  int     `json:"trackCount"`
-        Tracks      []Track `json:"tracks"`
+	Id          string  `json:"id"`
+	Title       string  `json:"title"`
+	Artist      string  `json:"artist"`
+	Cover       string  `json:"cover"`
+	ReleaseDate string  `json:"releaseDate"`
+	TrackCount  int     `json:"trackCount"`
+	Tracks      []Track `json:"tracks"`
 }
 
 func NewAlbum(albumId string) (*Album, error) {
-        type Response struct {
-                Album Album `json:"album"`
-        }
+	type Response struct {
+		Album Album `json:"album"`
+	}
 
-        res, err := _request("api/album", true, []QueryParams{
-                {Name: "albumId", Value: albumId},
-        })
+	res, err := _request("api/album", true, []QueryParams{
+		{Name: "albumId", Value: albumId},
+	})
 
-        if err != nil {
-                return nil, fmt.Errorf("cannot fetch album (reason: %w)", err)
-        }
-        defer res.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("cannot fetch album (reason: %w)", err)
+	}
+	defer res.Body.Close()
 
-        var response Response
-        err = json.NewDecoder(res.Body).Decode(&response)
+	var response Response
+	err = json.NewDecoder(res.Body).Decode(&response)
 
-        if err != nil {
-                return nil, fmt.Errorf("cannot decode result: %w", err)
-        }
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode result: %w", err)
+	}
 
-        if response.Album.Id == "0" {
-                return nil, fmt.Errorf("album not found")
-        }
+	if response.Album.Id == "0" {
+		return nil, fmt.Errorf("album not found")
+	}
 
-        return &response.Album, nil
+	return &response.Album, nil
 }
 
 func (album *Album) downloadAlbum(format int, rc RenderContext) error {
-        outputLocation := config.GetDownloadLocation()
+	outputLocation := config.GetDownloadLocation()
 
-        if !DirExists(outputLocation) {
-                return fmt.Errorf("specified location for file downloads doesn't exists")
-        }
+	if !DirExists(outputLocation) {
+		return fmt.Errorf("specified location for file downloads doesn't exists")
+	}
 
-        var rootFolder = fmt.Sprintf("%s/%s", outputLocation, SanitizeFilename(album.Artist))
+	var rootFolder = fmt.Sprintf("%s/%s", outputLocation, SanitizeFilename(album.Artist))
 
-        if !DirExists(rootFolder) {
-                os.Mkdir(rootFolder, 0755)
-        }
+	if !DirExists(rootFolder) {
+		os.Mkdir(rootFolder, 0755)
+	}
 
-        var albumLocation = fmt.Sprintf("%s/%s", rootFolder, SanitizeFilename(album.Title))
+	var albumLocation = fmt.Sprintf("%s/%s", rootFolder, SanitizeFilename(album.Title))
 
-        if DirExists(albumLocation) {
-                return fmt.Errorf("album directory already exists")
-        }
+	if DirExists(albumLocation) {
+		return fmt.Errorf("album directory already exists")
+	}
 
-        err := os.Mkdir(albumLocation, 0755)
-        if err != nil {
-                return fmt.Errorf("can't create dir %s", albumLocation)
-        }
+	err := os.Mkdir(albumLocation, 0755)
+	if err != nil {
+		return fmt.Errorf("can't create dir %s", albumLocation)
+	}
 
-        progressChan := make(chan int, len(album.Tracks))
+	progressChan := make(chan int, len(album.Tracks))
 
-        for i := range album.Tracks {
-                album.Tracks[i].TrackNumber = i + 1
-        }
+	for i := range album.Tracks {
+		album.Tracks[i].TrackNumber = i + 1
+	}
 
-        maxRetries := 3
-        maxConcurrent := 3
-        var failedTracks []Track
-        tracksToDownload := album.Tracks
+	maxRetries := 3
+	maxConcurrent := 3
+	var failedTracks []Track
+	tracksToDownload := album.Tracks
 
-        trackers := make([]*progress.Tracker, 0)
+	trackers := make([]*progress.Tracker, 0)
 
-        switch rc.Mode {
-        case ModeAlbumDownload:
-                pw := rc.Pw
+	switch rc.Mode {
+	case ModeAlbumDownload:
+		pw := rc.Pw
 
-                pw.SetNumTrackersExpected(len(tracksToDownload))
-                pw.Style().Visibility.TrackerOverall = true
+		pw.SetNumTrackersExpected(len(tracksToDownload))
+		pw.Style().Visibility.TrackerOverall = true
 
-                trackers = GetTrackersTrackSizes(tracksToDownload, format)
-                pw.AppendTrackers(trackers)
+		trackers = GetTrackersTrackSizes(tracksToDownload, format)
+		pw.AppendTrackers(trackers)
 
-                go pw.Render()
-        case ModeArtistDownload:
-                rc.Pw.SetNumTrackersExpected(len(tracksToDownload))
-        }
+		go pw.Render()
+	case ModeArtistDownload:
+		rc.Pw.SetNumTrackersExpected(len(tracksToDownload))
+	}
 
-        for i := range maxRetries {
-                if len(tracksToDownload) == 0 {
-                        break
-                }
+	for i := 0; i < maxRetries; i++ {
+		if len(tracksToDownload) == 0 {
+			break
+		}
 
-                if i > 0 {
-                        PrintColor(COLOR_YELLOW, "\nRetrying %d failed tracks (attempt %d/%d)...\n", len(tracksToDownload), i+1, maxRetries)
-                }
+		if i > 0 {
+			PrintColor(COLOR_YELLOW, "\nRetrying %d failed tracks (attempt %d/%d)...\n", len(tracksToDownload), i+1, maxRetries)
+		}
 
-                var wg sync.WaitGroup
-                sem := make(chan struct{}, maxConcurrent)
-                failedTracksChan := make(chan Track, len(tracksToDownload))
+		var wg sync.WaitGroup
+		sem := make(chan struct{}, maxConcurrent)
+		failedTracksChan := make(chan Track, len(tracksToDownload))
 
-                for idx, track := range tracksToDownload {
-                        wg.Add(1)
-                        sem <- struct{}{}
+		for idx, track := range tracksToDownload {
+			wg.Add(1)
+			sem <- struct{}{}
 
-                        var trk *progress.Tracker = nil
-                        if rc.Mode == ModeAlbumDownload {
-                                trk = trackers[idx]
-                        }
+			var trk *progress.Tracker = nil
+			if rc.Mode == ModeAlbumDownload {
+				trk = trackers[idx]
+			}
 
-                        go func(track Track, tk *progress.Tracker) {
-                                defer wg.Done()
-                                defer func() { <-sem }()
+			go func(track Track, tk *progress.Tracker) {
+				defer wg.Done()
+				defer func() { <-sem }()
 
-                                var trackName string
-                                if track.TrackNumber < 10 {
-                                        trackName = fmt.Sprintf("0%d - %s", track.TrackNumber, SanitizeFilename(track.Title))
-                                } else {
-                                        trackName = fmt.Sprintf("%d - %s", track.TrackNumber, SanitizeFilename(track.Title))
-                                }
+				var trackName string
+				if track.TrackNumber < 10 {
+					trackName = fmt.Sprintf("0%d - %s", track.TrackNumber, SanitizeFilename(track.Title))
+				} else {
+					trackName = fmt.Sprintf("%d - %s", track.TrackNumber, SanitizeFilename(track.Title))
+				}
 
-                                fileFormat := "flac"
+				fileFormat := "flac"
 
-                                if format == 5 {
-                                        fileFormat = "mp3"
-                                }
+				if format == 5 {
+					fileFormat = "mp3"
+				}
 
-                                location := fmt.Sprintf("%s/%s.%s", albumLocation, trackName, fileFormat)
-                                err := track.downloadTrack(location, format, tk)
+				location := fmt.Sprintf("%s/%s.%s", albumLocation, trackName, fileFormat)
+				err := track.downloadTrack(location, format, tk)
 
-                                if rc.Mode == ModeArtistDownload {
-                                        rc.Tracker.Increment(1)
-                                }
+				if rc.Mode == ModeArtistDownload {
+					rc.Tracker.Increment(1)
+				}
 
-                                if err != nil {
-                                        failedTracksChan <- track
-                                } else {
-                                        progressChan <- 1
-                                }
-                                time.Sleep(time.Duration(rand.Intn(1500)+500) * time.Millisecond)
-                        }(track, trk)
-                }
+				if err != nil {
+					failedTracksChan <- track
+				} else {
+					progressChan <- 1
+				}
+				time.Sleep(time.Duration(rand.Intn(1500)+500) * time.Millisecond)
+			}(track, trk)
+		}
 
-                wg.Wait()
-                close(failedTracksChan)
+		wg.Wait()
+		close(failedTracksChan)
 
-                failedTracks = nil
-                for failedTrack := range failedTracksChan {
-                        failedTracks = append(failedTracks, failedTrack)
-                }
+		failedTracks = nil
+		for failedTrack := range failedTracksChan {
+			failedTracks = append(failedTracks, failedTrack)
+		}
 
-                tracksToDownload = failedTracks
-        }
+		tracksToDownload = failedTracks
+	}
 
-        close(progressChan)
+	close(progressChan)
 
-        if len(failedTracks) > 0 {
-                var errorMessages []string
-                for _, track := range failedTracks {
-                        errorMessages = append(errorMessages, fmt.Sprintf("'%s' (ID: %d)", track.Title, track.Id))
-                }
-                return fmt.Errorf("completed with %d errors. Failed to download tracks: %s", len(failedTracks), errorMessages)
-        }
+	if len(failedTracks) > 0 {
+		var errorMessages []string
+		for _, track := range failedTracks {
+			errorMessages = append(errorMessages, fmt.Sprintf("'%s' (ID: %d)", track.Title, track.Id))
+		}
+		return fmt.Errorf("completed with %d errors. Failed to download tracks: %s", len(failedTracks), errorMessages)
+	}
 
-        return nil
+	return nil
 }
 
 func (album *Album) Download(format int, log bool) error {
-        if log {
-                PrintColor(COLOR_GREEN, "Starting download for album %s\n", album.Title)
-        }
+	if log {
+		PrintColor(COLOR_GREEN, "Starting download for album %s\n", album.Title)
+	}
 
-        pw := InitProgress()
-        rc := RenderContext{
-                Pw:   pw,
-                Mode: ModeAlbumDownload,
-        }
+	pw := InitProgress()
+	rc := RenderContext{
+		Pw:   pw,
+		Mode: ModeAlbumDownload,
+	}
 
-        err := album.downloadAlbum(format, rc)
+	err := album.downloadAlbum(format, rc)
 
-        if err != nil {
-                return fmt.Errorf("%w", err)
-        }
+	if err != nil {
+		return fmt.Errorf("%w", err)
+	}
 
-        return nil
+	return nil
 }
-
--- api/api.go
+++ api/api.go
@@ -1,329 +1,322 @@
 package api
 
 import (
-        "bytes"
-        "crypto/tls"
-        "encoding/json"
-        "errors"
-        "fmt"
-        "godab/config"
-        "io"
-        "net/http"
-        "net/http/cookiejar"
-        "net/url"
-        "os"
-        "path/filepath"
-        "strconv"
-        "strings"
-        "time"
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"godab/config"
+	"io"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
 
-        "go.senan.xyz/taglib"
+	"go.senan.xyz/taglib"
 )
 
 type ID int
 
 type AlbumsResults struct {
-        Items []Album `json:"albums"`
+	Items []Album `json:"albums"`
 }
 
 type TrackResults struct {
-        Items []Track `json:"tracks"`
+	Items []Track `json:"tracks"`
 }
 
 type ArtistResults struct {
-        Items []Artist
+	Items []Artist
 }
 
 type SearchResults struct {
-        Tracks  TrackResults
-        Albums  AlbumsResults
-        Artists ArtistResults
+	Tracks  TrackResults
+	Albums  AlbumsResults
+	Artists ArtistResults
 }
 
 type QueryParams struct {
-        Name  string
-        Value string
+	Name  string
+	Value string
 }
 
 type Metadatas struct {
-        Title       string
-        Artist      string
-        Album       string
-        Date        string
-        Cover       string
-        TrackNumber int
+	Title       string
+	Artist      string
+	Album       string
+	Date        string
+	Cover       string
+	TrackNumber int
 }
 
 var jar, _ = cookiejar.New(nil)
 var client = &http.Client{
-        Transport: &http.Transport{
-                TLSClientConfig: &tls.Config{
-                        MinVersion: tls.VersionTLS13,
-                },
-                IdleConnTimeout:       100000 * time.Second,
-                TLSHandshakeTimeout:   100000 * time.Second,
-                ExpectContinueTimeout: 100000 * time.Second,
-        },
-        Jar:     jar,
-        Timeout: 0,
+	Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{
+			MinVersion: tls.VersionTLS13,
+		},
+		IdleConnTimeout:       100000 * time.Second,
+		TLSHandshakeTimeout:   100000 * time.Second,
+		ExpectContinueTimeout: 100000 * time.Second,
+	},
+	Jar:     jar,
+	Timeout: 0,
 }
 
-
 func (id *ID) UnmarshalJSON(data []byte) error {
-        s := string(data)
-        s = strings.Trim(s, `"`)
-        val, _ := strconv.Atoi(s)
-        *id = ID(val)
-        return nil
+	s := string(data)
+	s = strings.Trim(s, `"`)
+	val, _ := strconv.Atoi(s)
+	*id = ID(val)
+	return nil
 }
 
 func LoadCookies() (bool, error) {
-        if _, err := os.Stat(".token"); errors.Is(err, os.ErrNotExist) {
-                return false, nil
-        }
+	if _, err := os.Stat(".token"); errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
 
-        path := filepath.Join(".token")
-        dat, err := os.ReadFile(path)
+	path := filepath.Join(".token")
+	dat, err := os.ReadFile(path)
 
-        if err != nil {
-                return false, fmt.Errorf("unable to read .token file")
-        }
+	if err != nil {
+		return false, fmt.Errorf("unable to read .token file")
+	}
 
-        url, _ := url.Parse(config.GetEndpoint())
-        client.Jar.SetCookies(url, []*http.Cookie{
-                {
-                        Name:  "session",
-                        Value: string(dat),
-                },
-        })
+	url, _ := url.Parse(config.GetEndpoint())
+	client.Jar.SetCookies(url, []*http.Cookie{
+		{
+			Name:  "session",
+			Value: string(dat),
+		},
+	})
 
-        return dat != nil, nil
+	return dat != nil, nil
 }
 
 func _request(path string, isPathOnly bool, params []QueryParams) (resp *http.Response, err error) {
-        var fullUrl string
+	var fullUrl string
 
-        if isPathOnly {
-                fullUrl = fmt.Sprintf("%s/%s", config.GetEndpoint(), path)
+	if isPathOnly {
+		fullUrl = fmt.Sprintf("%s/%s", config.GetEndpoint(), path)
 
-                u, err := url.Parse(fullUrl)
+		u, err := url.Parse(fullUrl)
 
-                if err != nil {
-                        return nil, fmt.Errorf("cannot parse url %s", fullUrl)
-                }
+		if err != nil {
+			return nil, fmt.Errorf("cannot parse url %s", fullUrl)
+		}
 
-                q := u.Query()
-                for _, queryParam := range params {
-                        q.Set(queryParam.Name, queryParam.Value)
-                }
-                u.RawQuery = q.Encode()
+		q := u.Query()
+		for _, queryParam := range params {
+			q.Set(queryParam.Name, queryParam.Value)
+		}
+		u.RawQuery = q.Encode()
 
-                fullUrl = u.String()
-        } else {
-                fullUrl = path
-        }
+		fullUrl = u.String()
+	} else {
+		fullUrl = path
+	}
 
-        req, err := http.NewRequest(http.MethodGet, fullUrl, nil)
-        if err != nil {
-                return nil, fmt.Errorf("can't create request: %w", err)
-        }
+	req, err := http.NewRequest(http.MethodGet, fullUrl, nil)
+	if err != nil {
+		return nil, fmt.Errorf("can't create request: %w", err)
+	}
 
-        req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36")
 
-        res, err := client.Do(req)
+	res, err := client.Do(req)
 
-        if err != nil {
-                return nil, fmt.Errorf("can't fetch endpoint %s: %w", fullUrl, err)
-        }
+	if err != nil {
+		return nil, fmt.Errorf("can't fetch endpoint %s: %w", fullUrl, err)
+	}
 
-        if res.StatusCode != http.StatusOK {
-                return nil, fmt.Errorf("request to %s failed with status code: %s", fullUrl, res.Status)
-        }
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request to %s failed with status code: %s", fullUrl, res.Status)
+	}
 
-        return res, nil
+	return res, nil
 }
 
 func _addMetadata(targetFile string, metadatas Metadatas) error {
-        res, err := _request(metadatas.Cover, false, []QueryParams{})
+	res, err := _request(metadatas.Cover, false, []QueryParams{})
 
-        if err != nil {
-                return fmt.Errorf("can't download cover")
-        }
+	if err != nil {
+		return fmt.Errorf("can't download cover")
+	}
 
-        defer res.Body.Close()
+	defer res.Body.Close()
 
-        coverBytes, err := io.ReadAll(res.Body)
-        if err != nil {
-                panic(err)
-        }
+	coverBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
 
-        err = taglib.WriteTags(targetFile, map[string][]string{
-                taglib.Title:  {metadatas.Title},
-                taglib.Artist: {metadatas.Artist},
-                taglib.Album:  {metadatas.Album},
-                taglib.Date:   {metadatas.Date},
-        }, 0)
+	err = taglib.WriteTags(targetFile, map[string][]string{
+		taglib.Title:  {metadatas.Title},
+		taglib.Artist: {metadatas.Artist},
+		taglib.Album:  {metadatas.Album},
+		taglib.Date:   {metadatas.Date},
+	}, 0)
 
-        if err != nil {
-                return fmt.Errorf("unable to write metadata to track")
-        }
+	if err != nil {
+		return fmt.Errorf("unable to write metadata to track")
+	}
 
-        if len(coverBytes) > 0 {
-                err = taglib.WriteImage(targetFile, coverBytes)
-        }
+	if len(coverBytes) > 0 {
+		err = taglib.WriteImage(targetFile, coverBytes)
+	}
 
-        if err != nil {
-                return fmt.Errorf("unable to picture meadata to track")
-        }
+	if err != nil {
+		return fmt.Errorf("unable to picture meadata to track")
+	}
 
-        return nil
+	return nil
 }
 
 func Login(email string, password string) error {
-        if email == "" || password == "" {
-                return fmt.Errorf("invalid email or password")
-        }
+	if email == "" || password == "" {
+		return fmt.Errorf("invalid email or password")
+	}
 
-        body := struct {
-                Email    string `json:"email"`
-                Password string `json:"password"`
-        }{
-                Email:    email,
-                Password: password,
-        }
+	body := struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}{
+		Email:    email,
+		Password: password,
+	}
 
-        out, err := json.Marshal(body)
-        if err != nil {
-                return fmt.Errorf("cannot encode login body")
-        }
+	out, err := json.Marshal(body)
+	if err != nil {
+		return fmt.Errorf("cannot encode login body")
+	}
 
-        res, err := http.Post(
-                fmt.Sprintf("%s/%s", config.GetEndpoint(), "api/auth/login"),
-                "application/json",
-                bytes.NewBuffer(out),
-        )
+	res, err := http.Post(
+		fmt.Sprintf("%s/%s", config.GetEndpoint(), "api/auth/login"),
+		"application/json",
+		bytes.NewBuffer(out),
+	)
 
-        if res.StatusCode == 401 {
-                return fmt.Errorf("invalid credentials")
-        }
+	if res.StatusCode == 401 {
+		return fmt.Errorf("invalid credentials")
+	}
 
-        if res.StatusCode == 200 {
-                var token = ""
-                for _, cook := range res.Cookies() {
-                        if cook.Name == "session" {
-                                token = cook.Value
-                        }
-                }
+	if res.StatusCode == 200 {
+		var token = ""
+		for _, cook := range res.Cookies() {
+			if cook.Name == "session" {
+				token = cook.Value
+			}
+		}
 
-                if token == "" {
-                        return fmt.Errorf("unable to get token")
-                }
+		if token == "" {
+			return fmt.Errorf("unable to get token")
+		}
 
-                path := filepath.Join(".token")
-                err := os.WriteFile(path, []byte(token), 0644)
+		path := filepath.Join(".token")
+		err := os.WriteFile(path, []byte(token), 0644)
 
-                if err != nil {
-                        return fmt.Errorf("unable to write .token file")
-                }
+		if err != nil {
+			return fmt.Errorf("unable to write .token file")
+		}
 
-                url, _ := url.Parse(config.GetEndpoint())
-                client.Jar.SetCookies(url, []*http.Cookie{
-                        {
-                                Name:  "session",
-                                Value: token,
-                        },
-                })
+		url, _ := url.Parse(config.GetEndpoint())
+		client.Jar.SetCookies(url, []*http.Cookie{
+			{
+				Name:  "session",
+				Value: token,
+			},
+		})
+	}
 
-        }
+	if err != nil {
+		return fmt.Errorf("error while making request: %w", err)
+	}
 
-        if err != nil {
-                return fmt.Errorf("error while making request: %w", err)
-        }
-
-        return nil
+	return nil
 }
 
 func Search(query string, queryType string) (*SearchResults, error) {
-        if query == "" {
-                return nil, fmt.Errorf("you must provide a valid query parameter")
-        }
+	if query == "" {
+		return nil, fmt.Errorf("you must provide a valid query parameter")
+	}
 
-        if queryType != "album" && queryType != "track" && queryType != "artist" {
-                return nil, fmt.Errorf("you must provide a queryType of either type `track`, `album` or `artist`")
-        }
+	if queryType != "album" && queryType != "track" && queryType != "artist" {
+		return nil, fmt.Errorf("you must provide a queryType of either type `track`, `album` or `artist`")
+	}
 
-        res, err := _request("api/search", true, []QueryParams{
-                {Name: "q", Value: query},
-                {Name: "type", Value: queryType},
-        })
+	res, err := _request("api/search", true, []QueryParams{
+		{Name: "q", Value: query},
+		{Name: "type", Value: queryType},
+	})
 
-        if err != nil {
-                return nil, fmt.Errorf("search endpoint failed: %s", err)
-        }
-        defer res.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("search endpoint failed: %s", err)
+	}
+	defer res.Body.Close()
 
-        var searchResponse SearchResults
+	var searchResponse SearchResults
 
-        switch queryType {
-        case "album":
+	switch queryType {
+	case "album":
+		var albums AlbumsResults
+		err = json.NewDecoder(res.Body).Decode(&albums)
 
-                var albums AlbumsResults
-                err = json.NewDecoder(res.Body).Decode(&albums)
+		if err != nil {
+			return nil, fmt.Errorf("cannot decode response: %w", err)
+		}
 
-                if err != nil {
-                        return nil, fmt.Errorf("cannot decode response: %w", err)
-                }
+		searchResponse.Albums = albums
+	case "track":
+		var response TrackResults
+		err = json.NewDecoder(res.Body).Decode(&response)
 
-                searchResponse.Albums = albums
-        case "track":
-                var response TrackResults
-                err = json.NewDecoder(res.Body).Decode(&response)
+		if err != nil {
+			return nil, fmt.Errorf("cannot decode response: %w", err)
+		}
 
-                if err != nil {
-                        return nil, fmt.Errorf("cannot decode response: %w", err)
-                }
+		searchResponse.Tracks = response
+	case "artist":
+		artists := make(map[float64]Artist)
+		rawJSON := make(map[string]any)
 
-                searchResponse.Tracks = response
-        case "artist":
-                // var response ArtistResults
+		err = json.NewDecoder(res.Body).Decode(&rawJSON)
 
-                artists := make(map[float64]Artist)
-                rawJSON := make(map[string]any)
+		if err != nil {
+			return nil, fmt.Errorf("cannot decode response: %w", err)
+		}
 
-                err = json.NewDecoder(res.Body).Decode(&rawJSON)
+		var tracks = rawJSON["tracks"].([]any)
 
-                if err != nil {
-                        return nil, fmt.Errorf("cannot decode response: %w", err)
-                }
+		for _, t := range tracks {
+			t, ok := t.(map[string]any)
+			if !ok {
+				return nil, fmt.Errorf("track item is not an object")
+			}
+			var artistId float64 = t["artistId"].(float64)
+			_, ok = artists[artistId]
+			if artistId != 0 && !ok {
+				id := ID(artistId)
+				artists[artistId] = Artist{
+					Id:   id,
+					Name: t["artist"].(string),
+				}
+			}
+		}
 
-                var tracks = rawJSON["tracks"].([]any)
+		artistsSlice := make([]Artist, 0, len(artists))
+		for _, artist := range artists {
+			artistsSlice = append(artistsSlice, artist)
+		}
+		searchResponse.Artists = ArtistResults{
+			Items: artistsSlice,
+		}
+	}
 
-                for _, t := range tracks {
-                        t, ok := t.(map[string]any)
-                        if !ok {
-                                return nil, fmt.Errorf("track item is not an object")
-                        }
-                        var artistId float64 = t["artistId"].(float64)
-                        _, ok = artists[artistId]
-                        if artistId != 0 && !ok {
-                                id := ID(artistId)
-                                artists[artistId] = Artist{
-                                        Id:   id,
-                                        Name: t["artist"].(string),
-                                }
-                        }
-                }
-
-                artistsSlice := make([]Artist, 0, len(artists))
-                for _, artist := range artists {
-                        artistsSlice = append(artistsSlice, artist)
-                }
-                searchResponse.Artists = ArtistResults{
-                        Items: artistsSlice,
-                }
-
-        }
-
-        return &searchResponse, nil
+	return &searchResponse, nil
 }
-
